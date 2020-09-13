ce79648b469f616ff2eb955656b2e39249db29c9
lavomatyc <matias.ceau@protonmail.com>
2020-09-13 19:08:06 +0200

    rsync stuuf

--- .320stoHDD.sh
+++ .320stoHDD.sh
@@ -1,3 +1,2 @@
 #! /bin/sh
-mount -U bbbffaac-2da5-4eb4-966f-c9573e4b7f38 /home/matias/Media
-rsync -a --info=progress2 --exclude /home/matias/Media /home/matias/ /home/matias/Media/320s
+rsync -a --info=progress2 --exclude 'Media' /home/matias/ /home/matias/Media/320s
--- .h270hd3toHDD.sh
+++ .h270hd3toHDD.sh
@@ -1,3 +1,2 @@
 #! /bin/sh
-mount -U bbbffaac-2da5-4eb4-966f-c9573e4b7f38 /home/matias/Media
-rsync -a --info=progress2 --exclude={/home/lavomatyc/Media,/home/lavomatyc/PrimaryData} /home/lavomatyc/ /home/lavomatyc/Media/h270hd3
+rsync -a --info=progress2 --exclude={Media,PrimaryData} /home/lavomatyc/ /home/lavomatyc/Media/h270hd3
